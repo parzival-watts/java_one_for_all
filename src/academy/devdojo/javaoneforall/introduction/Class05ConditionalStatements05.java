31e38c25c56158dc07cb27aa3908c546734abb90
parzival-watts <dylan.philliplitz@gmail.com>
2024-03-04 15:53:18 -0300

    Add Class05 Conditional Statements Switch part 01

---
+++ src/academy/devdojo/javaoneforall/introduction/Class05ConditionalStatements05.java
@@ -0,0 +1,34 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class05ConditionalStatements05 {
+    public static void main(String[] args) {
+        byte day = 5;
+
+        // char, int, byte, short, enum, String
+        switch(day){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tue");
+                break;
+            case 4:
+                System.out.println("Wed");
+                break;
+            case 5:
+                System.out.println("Thu");
+                break;
+            case 6:
+                System.out.println("Frid");
+                break;
+            case 7:
+                System.out.println("Sat");
+                break;
+
+        }
+
+    }
+}
