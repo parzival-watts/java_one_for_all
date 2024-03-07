0bc8036358ebfeefdc388adae00f29380c84f4f7
parzival-watts <dylan.philliplitz@gmail.com>
2024-03-07 15:21:35 -0300

    arrays

---
+++ src/academy/devdojo/javaoneforall/introduction/Class07Arrays01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class07Arrays01 {
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+        ages[0] = 12;
+        System.out.println(ages[0]);
+    }
+}
---
+++ src/academy/devdojo/javaoneforall/introduction/Class07Arrays02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, double, float, long, int = 0
+        // char '\u0000' ''
+        // boolean false
+        // String null
+        int[] ages = new int[3];
+
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+    }
+}
