1d7f06ea73a1a8cd7055f108b9f174eb46757b7e
parzival-watts <dylan.philliplitz@gmail.com>
2024-03-07 15:18:57 -0300

    Add Class06 Loop Statements Part 03 And 04

---
+++ src/academy/devdojo/javaoneforall/introduction/Class06LoopStatements04.java
@@ -0,0 +1,26 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class06LoopStatements04 {
+    /*
+        Given a car's total price, for example 20000
+        I want to find how much and for how long I'll have to pay monthly.
+        Condition: the monthly paymenty can not be lower than 1000
+    */
+    public static void main(String[] args) {
+        double totalPrice = 20000;
+        for (int monthCount = 1; monthCount < 20000; monthCount++) {
+            double monthlyPrice = totalPrice / monthCount;
+            if(monthlyPrice < 1000){
+                break;
+            }
+            System.out.println("Months " + monthCount + " Price: " + monthlyPrice);
+
+//            if(monthlyPrice >= 1000){
+//                System.out.println("Months " + monthCount + " Price: " + monthlyPrice);
+//            }else{
+//                break;
+//            }
+//            System.out.println("Inside for, outside if " + monthCount);
+        }
+    }
+}
