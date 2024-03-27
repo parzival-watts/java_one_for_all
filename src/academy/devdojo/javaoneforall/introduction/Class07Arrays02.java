22e7ae99e9de64cf9e13e892de2876f945290739
PhillDylan <dylan.philliplitz@gmail.com>
2024-03-26 23:17:44 -0300

    arrays part 03

--- src/academy/devdojo/javaoneforall/introduction/Class07Arrays02.java
+++ src/academy/devdojo/javaoneforall/introduction/Class07Arrays02.java
@@ -7,9 +7,12 @@ public class Class07Arrays02 {
         // boolean false
         // String null
         int[] ages = new int[3];
+        ages[0] = 23;
+        ages[1] = 24;
+        ages[2] = 25;
 
-        System.out.println(ages[0]);
-        System.out.println(ages[1]);
-        System.out.println(ages[2]);
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
+        }
     }
 }
---
+++ src/academy/devdojo/javaoneforall/introduction/Class07Arrays03.java
@@ -0,0 +1,18 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class07Arrays03 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        int[] numbers2 = {1,2,3,4,5};
+        int[] numbers3 = new int[] {1,2,3,4,5};
+
+        for (int i = 0; i < numbers2.length; i++) {
+            System.out.println(numbers2[i]);
+        }
+
+        for(int num: numbers3){
+            System.out.println(num);
+        }
+
+    }
+}
