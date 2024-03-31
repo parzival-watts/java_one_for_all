592f9eaaf98ce994317519a30e91de486771bbd6
PhillDylan <dylan.philliplitz@gmail.com>
2024-03-31 12:28:03 -0300

    Methods pt 05 primitive type parameters

--- src/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
+++ src/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
@@ -35,4 +35,12 @@ public class Calculator {
         }
         System.out.println(num1 / num2);
     }
+
+    public void changeTwoNumbers(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Inside changeTwoNumbers");
+        System.out.println("Num1: " + num1);
+        System.out.println("Num2: " + num2);
+    }
 }
